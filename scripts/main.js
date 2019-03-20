bdf0491a55b370762626548c4d997eb2b94b4fb0
shobbs <mail.shobbs@gmail.com>
2019-03-20 10:53:07 -0700

    testing

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,6 @@
 var myHeading = document.querySelector('h1');
 myHeading.textContent = 'Hello world!';
-var r = new XMLHttpRequest();
-r.open("POST", "path/to/api", true);
-r.onreadystatechange = function () {
-  if (r.readyState != 4 || r.status != 200) return;
-  alert("Success: " + r.responseText);
-};
-r.send("banana=yellow");
+var shopping = ['bread', 'milk', 'cheese', 'hummus', 'noodles'];
 var sequence = [1, 1, 2, 3, 5, 8, 13];
 var random = ['tree', 795, [0, 1, 2]];
 shopping[0];
